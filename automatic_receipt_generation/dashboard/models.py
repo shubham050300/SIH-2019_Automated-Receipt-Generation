19b50da8337c95d74c1af6e9902ceff195b0c4fc
Shreyans Dubey <theshreyansdubey@gmail.com>
2019-03-02 09:35:22 +0530

    updated

--- automatic_receipt_generation/dashboard/models.py
+++ automatic_receipt_generation/dashboard/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class SAPfile(models.Model) :
     sample_SAP = models.FileField(upload_to='SAP/')
+    desc = models.CharField(max_length = 1024)
     
     def __str__(self) :
-        return 
+        return self.desc
