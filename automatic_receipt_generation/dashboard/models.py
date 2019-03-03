9c8f097446e09c6c09279c0a53dbe222dcc655fd
Shreyans Dubey <theshreyansdubey@gmail.com>
2019-03-03 14:30:58 +0530

    Updated

--- automatic_receipt_generation/dashboard/models.py
+++ automatic_receipt_generation/dashboard/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class SAPfile(models.Model) :
-    Cheque_SAP = models.FileField(upload_to='SAP/')
-    Card_SAP = models.FileField(upload_to='SAP/')
-    OCR_image = models.ImageField(upload_to='OCR/')
-    desc = models.CharField(max_length = 1024, default = 'SAP file')
+    Cheque_SAP = models.FileField(upload_to='SAP/', blank = True)
+    Card_SAP = models.FileField(upload_to='SAP/', blank = True)
+    OCR_image = models.ImageField(upload_to='OCR/', blank = True)
+    desc = models.CharField(max_length = 1024, blank = True)
     
     def __str__(self) :
         return self.desc
